1219ce9626aaf4889d57985d0e5e300633de5409
West, Abby <abbyrosewest@gmail.com>
2019-03-01 15:59:15 -0500

    Bot is comming on/offline properly

--- DoctorWh0oDiscordBot/Program.cs
+++ DoctorWh0oDiscordBot/Program.cs
@@ -1,20 +1,64 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
 
 using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace DoctorWh0oDiscordBot
 {
     class Program
     {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider Services;
+
         static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
 
         private async Task MainAsync()
         {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
 
+            Services = new ServiceCollection().AddSingleton(Client).AddSingleton(Commands).BuildServiceProvider();
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            await Client.LoginAsync(TokenType.Bot, "NTUxMTEzMTI2NTc4MjI1MTk0.D1sQRw.RkhWvwkiay3kqBcX83DjjOlROr8");
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task Client_Ready()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task Client_MessageReceived(SocketMessage arg)
+        {
+            throw new NotImplementedException();
         }
     }
 }
