8af6377c7149708ccf954aae4a30646af84a92d2
Wessels <abbywessels@quickenloans.com>
2019-03-01 14:25:58 -0500

    Add project files.

---
+++ DoctorWh0oDiscordBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DoctorWh0oDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+
+        }
+    }
+}
