4fa126d0ad7feb94a60fce1f46273d357f92888e
West, Abby <abbyrosewest@gmail.com>
2019-03-11 14:38:50 -0400

    adding token security

---
+++ DoctorWh0oDiscordBot/Core/Commands/Greeting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Commands;
+using System.Reflection;
+using System.IO;
+
+namespace DoctorWh0oDiscordBot.Core.Commands
+{
+    class Greeting : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello"), Alias("helloworld", "world"), Summary("Hello world command")]
+        public async Task HelloWorld()
+        {
+            await Context.Channel.SendMessageAsync("Hello World");
+        }
+    }
+}
--- DoctorWh0oDiscordBot/Program.cs
+++ DoctorWh0oDiscordBot/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
 
 using Discord;
-using Discord.WebSocket;
 using Discord.Commands;
-using Microsoft.Extensions.DependencyInjection;
+using Discord.WebSocket;
 
+using DoctorWh0oDiscordBot.Resources.Datatypes;
+using DoctorWh0oDiscordBot.Resources.Settings;
 
 namespace DoctorWh0oDiscordBot
 {
@@ -14,19 +20,32 @@ namespace DoctorWh0oDiscordBot
     {
         private DiscordSocketClient Client;
         private CommandService Commands;
-        private IServiceProvider Services;
 
-        static void Main(string[] args)
-        => new Program().MainAsync().GetAwaiter().GetResult();
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         private async Task MainAsync()
         {
+            //string JSON = "";
+            //string SettingsLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.0", @"Data\Settings.json");
+            //using (var Stream = new FileStream(SettingsLocation, FileMode.Open, FileAccess.Read))
+            //using (var ReadSettings = new StreamReader(Stream))
+            //{
+            //    JSON = ReadSettings.ReadToEnd();
+            //}
+
+            //Setting Settings = JsonConvert.DeserializeObject<Setting>(JSON);
+            //ESettings.Banned = Settings.banned;
+            //ESettings.Log = Settings.log;
+            //ESettings.Owner = Settings.owner;
+            //ESettings.Token = Settings.token;
+            //ESettings.Version = Settings.version;
+
             Client = new DiscordSocketClient(new DiscordSocketConfig
             {
-                LogLevel = LogSeverity.Debug
+                LogLevel = LogSeverity.Info
             });
 
-            Services = new ServiceCollection().AddSingleton(Client).AddSingleton(Commands).BuildServiceProvider();
+            //help help HELP heLp
             Commands = new CommandService(new CommandServiceConfig
             {
                 CaseSensitiveCommands = true,
@@ -35,30 +54,52 @@ namespace DoctorWh0oDiscordBot
             });
 
             Client.MessageReceived += Client_MessageReceived;
-            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+            await Commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
 
             Client.Ready += Client_Ready;
             Client.Log += Client_Log;
 
-            await Client.LoginAsync(TokenType.Bot, "NTUxMTEzMTI2NTc4MjI1MTk0.D1sQRw.RkhWvwkiay3kqBcX83DjjOlROr8");
+
+            ESettings.Token = File.ReadLines(@"C:\Users\awessels\source\repos\DoctorWh0oDiscordBot\.gitignore").Skip(263).Take(1).First();
+
+
+                await Client.LoginAsync(TokenType.Bot, ESettings.Token);
             await Client.StartAsync();
 
             await Task.Delay(-1);
         }
 
-        private async Task Client_Log(LogMessage arg)
+        private async Task Client_Log(LogMessage Message)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"{ DateTime.Now} at {Message.Source}] {Message.Message}");
+            try
+            {
+                SocketGuild Guild = Client.Guilds.Where(x => x.Id == ESettings.Log[0]).FirstOrDefault();
+                SocketTextChannel Channel = Guild.Channels.Where(x => x.Id == ESettings.Log[1]).FirstOrDefault() as SocketTextChannel;
+                await Channel.SendMessageAsync($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+            }
+            catch { }
         }
 
         private async Task Client_Ready()
         {
-            throw new NotImplementedException();
+            //await Client.SetActivityAsync("Owl Games");
         }
 
-        private Task Client_MessageReceived(SocketMessage arg)
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
         {
-            throw new NotImplementedException();
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+            if (!(Message.HasStringPrefix("a!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos, services: null);
+            if (!Result.IsSuccess)
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
         }
     }
 }
